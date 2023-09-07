69ce8cba5313660336986d24f78bdad7d738f0b5
chandlerliuliu <chandlerliu@tmrmds.co>
2023-09-07 14:10:03 +0800

    pl_onehot: remove groupby and index_col

--- polars/onehot_survey.py
+++ polars/onehot_survey.py
@@ -1,24 +1,19 @@
 import polars as pl
 
 def onehot_multichoices(
-    data: pl.DataFrame, index_col: str, multichoice_cols: list[str], pattern: str
+    data: pl.DataFrame, multichoice_cols: list[str], pattern: str
 ) -> pl.DataFrame:
     multichoice_dfs = []
     for col in multichoice_cols:
-        col_df = data.select(
-            index_col,
-            pl.col(col).str.split(pattern)
-        )
+        col_df = data.select(pl.col(col).str.split(pattern))
         multichoice_dfs.append(
             col_df
-            .group_by(index_col)
-            .agg(
-                pl.col(col).list.contains(val).first().cast(pl.Int8).suffix(f"__{val}")
+            .select(
+                pl.col(col).list.contains(val).cast(pl.Int8).suffix(f"__{val}")
                 for val in col_df.explode(col)[col].unique().to_list()
-                )
-            .select(pl.all().exclude(index_col))
+            )
         )
-    return pl.concat([data] + multichoice_dfs, how="horizontal")
+    return pl.concat([data.select(pl.all().exclude(multichoice_cols))] + multichoice_dfs, how="horizontal")
 
 
 # (
